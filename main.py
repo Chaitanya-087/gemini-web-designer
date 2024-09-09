d007ee9ac7050a7c005c94a2875262d6ab3f2c50
ssparser <reddyksidharth@gmail.com>
2024-09-09 16:22:33 +0530

    fixed history

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate,  MessagesPlaceholder
 from langchain_core.messages import HumanMessage
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_community.chat_message_histories import RedisChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_redis import RedisChatMessageHistory
+
+
 
 
 from dotenv import load_dotenv
@@ -13,8 +16,8 @@ API_KEY=os.getenv("GEMINI_API_KEY")
 REDIS_URL = "redis://localhost:6379/0"
 
 #session history
-def get_session_history(session_id):
-    return RedisChatMessageHistory(session_id, url=REDIS_URL)
+def get_redis_history(session_id: str) -> BaseChatMessageHistory:
+    return RedisChatMessageHistory(session_id, redis_url=REDIS_URL)
 
 # create model
 generation_config = {
@@ -34,7 +37,9 @@ prompt = ChatPromptTemplate.from_messages(
             # "you are a web developer who gives html and css code based on the user's request only give html and css no comments no intro no explanation",
             "you are a friend"
         ),
-        ("user", "{input}")
+        MessagesPlaceholder(variable_name="history"),
+
+        ("user", "{input}"),
     ]
 )
 
@@ -43,10 +48,15 @@ chain = prompt | model
 
 runnable_with_history = RunnableWithMessageHistory(
     chain,
-    get_session_history,
+    get_redis_history,
     input_messages_key="input",
     history_messages_key="history",
 )
 
 # run chain
-print(runnable_with_history.invoke({"input":"wat is my name"}, config={"configurable": {"session_id": "1"}}))
+response1 = runnable_with_history.invoke(
+    {"input": "wat is my name"},
+    config={"configurable": {"session_id": "2"}},
+)
+print("AI Response 1:", response1.content)
+
