04afad035e616a9a75a3ed63fd8b88e66f58b76c
Chaitanya-087 <sparkscj234@gmail.com>
2024-10-15 13:43:16 +0530

    removed useless code

--- app/util/chat.py
+++ app/util/chat.py
@@ -4,8 +4,3 @@ from datetime import datetime
 def generateUUID():
     salt = datetime.now().strftime("%Y%m%d%H%M%S")
     return str(uuid.uuid4()) + salt
-
-#driver code
-if __name__ == '__main__':
-    id = generateUUID()
-    print(id)
